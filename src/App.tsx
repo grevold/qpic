25f0959b8b56de0d7e51b9fa9105188a24195374
grevold <87669945+grevold@users.noreply.github.com>
2023-05-13 15:10:30 +0300

    Настроил React Router (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { RoutePath } from "./types";
+import { CartPage } from "./pages/CartPage/CartPage";
+import { MainPage } from "./pages/MainPage/MainPage";
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route element={<CartPage />} path={RoutePath.CartPage} />
+        <Route element={<MainPage />} path={RoutePath.MainPage} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
