0d5ccb7a87c1c8b27a533225401bcdf6b80dac66
Алексей <87669945+grevold@users.noreply.github.com>
2023-08-11 18:34:03 +0300

    Добавил музыку (#10)

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,3 +1,45 @@
+import s from "./FavoritesPage.module.css";
+
 export const FavoritesPage = () => {
-  return <h1>FavoritesPage</h1>;
+  function playKick() {
+    let audio = new Audio(
+      `${process.env.PUBLIC_URL}/Samples/Drums/Kick_02.mp3`
+    );
+    audio.play();
+  }
+  function playHat() {
+    let audio = new Audio(`${process.env.PUBLIC_URL}/Samples/Drums/Hat_01.mp3`);
+    audio.play();
+  }
+  function playClap() {
+    let audio = new Audio(
+      `${process.env.PUBLIC_URL}/Samples/Drums/Clap_06.mp3`
+    );
+    audio.play();
+  }
+  function playBass() {
+    let audio = new Audio(
+      `${process.env.PUBLIC_URL}/Samples/Drums/Bass_01.mp3`
+    );
+    audio.play();
+  }
+
+  return (
+    <div className={s.root_music}>
+      <div className={s.playground}>
+        <button className={s.sound} onClick={playKick}>
+          Kick
+        </button>
+        <button className={s.sound} onClick={playHat}>
+          Hat
+        </button>
+        <button className={s.sound} onClick={playClap}>
+          Clap
+        </button>
+        <button className={s.sound} onClick={playBass}>
+          Bass
+        </button>
+      </div>
+    </div>
+  );
 };
